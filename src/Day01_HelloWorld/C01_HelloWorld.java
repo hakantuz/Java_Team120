3020f743178b6d6ad7338458cfa8b22797657e0c
Hakan TÜZ <hakantr02@gmail.com>
2023-10-23 15:48:25 +0300

    .

---
+++ src/Day01_HelloWorld/C01_HelloWorld.java
@@ -0,0 +1,11 @@
+package Day01_HelloWorld;
+
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello WiseQuarter");
+
+
+    }
+}
